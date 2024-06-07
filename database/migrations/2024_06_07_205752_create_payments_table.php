a09b5891ee096d42d7ee8eedc179a610597832a6
FTyumin <71492298+FTyumin@users.noreply.github.com>
2024-06-08 00:02:25 +0300

    Added remaining migrations

---
+++ database/migrations/2024_06_07_205752_create_payments_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('payments', function (Blueprint $table) {
+            $table->id();
+            $table->integer('reservation_id');
+            $table->integer('amount');
+            $table->date('date');
+            $table->string('payment_method');
+            $table->string('status');
+            $table->foreign('reservation_id')->references('id')->on('reservations');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('payments');
+    }
+};
