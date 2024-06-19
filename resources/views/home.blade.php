1024f68822e960463ae6fd5a65b9e5c5b0c4bc25
FTyumin <71492298+FTyumin@users.noreply.github.com>
2024-06-19 11:20:39 +0300

    Edits

---
+++ resources/views/home.blade.php
@@ -0,0 +1,10 @@
+{{-- @extends('layouts.app') --}}
+
+@section('content')
+    <h1>Home</h1>
+    <p>Welcome to the Hotel Reservation System</p>
+@endsection
+
+
+<h1>Home</h1>
+    <p>Welcome to the Hotel Reservation System</p>
